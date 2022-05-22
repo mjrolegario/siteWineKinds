660125ac0fb7310f99f0450b799b96865d1ea541
MANOEL OLEGÁRIO O. JR <manoell_junior@hotmail.com>
2022-05-22 00:03:36 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+jQuery("#iniciobut").mousemove (()=>{	
+	jQuery("#iniciobut").css("font-size", "21px")
+})
+jQuery('#iniciobut').mouseleave (()=>{	
+	jQuery("#iniciobut").css("font-size", "20px")
+})
+jQuery("#catalogobut").mousemove (()=>{	
+	jQuery("#catalogobut").css("font-size", "21px")
+})
+jQuery('#catalogobut').mouseleave (()=>{	
+	jQuery("#catalogobut").css("font-size", "20px")
+})
+jQuery("#sugestoesbut").mousemove (()=>{	
+	jQuery("#sugestoesbut").css("font-size", "21px")
+})
+jQuery('#sugestoesbut').mouseleave (()=>{	
+	jQuery("#sugestoesbut").css("font-size", "20px")
+})
